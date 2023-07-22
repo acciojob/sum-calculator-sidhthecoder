17f79dd56d8509935d94358c41f74cbe5c3b59a2
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-07-22 12:01:12 +0530

    submitting code-Sat Jul 22 2023 12:01:11 GMT+0530 (India Standard Time)

---
+++ src/components/Calculator.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Calculator = () => {
+  const [number, setNumber] = useState(0);
+  const [sum, setSum] = useState(0);
+
+  function addSum(e) {
+    const value = parseFloat(e.target.value);
+    setNumber(value);
+    Sum(value);
+  }
+
+  function Sum(value) {
+    // Calculate the sum of all numbers (previous numbers + new value)
+    setSum((prevSum) => prevSum + value);
+  }
+
+  return (
+    <div>
+      <h1>Sum Calculator</h1>
+      <input type="number" onChange={addSum} />
+      <p>Sum:{sum}</p>
+    </div>
+  );
+};
+
+export default Calculator;
